3b44ca7636b11f75c9483ba01ce083958470c9f4
Gabrielgg-lan <gabrielpotatohead@gmail.com>
2025-09-18 13:06:53 -0300

    Add files via upload

---
+++ mario.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Altura: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 0; j < height - i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
